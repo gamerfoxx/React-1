e519da1733819c1c724b5f8f334546cc88fefca1
gamerfoxx <admin@josephludbrook.com>
2023-08-10 16:20:56 -0400

    two way binding

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './NewGoal.css';
 
 const NewGoal = (props) => {
+  const [newGoalLvlEnteredText, setNewGoalLvlEnteredText] = useState('');
   const addGoalHandler = (event) =>{
     event.preventDefault();
 
     const lNewGoal = {
       id: Math.random().toString(),
-      text: 'new goal'
+      text: newGoalLvlEnteredText
     }
 
     props.onAddGoal(lNewGoal);
+    setNewGoalLvlEnteredText('')
+  }
+
+  const textChangeHandler = (event) =>{
+    setNewGoalLvlEnteredText(event.target.value);
   }
 
   return (
   <form className='new-goal' onSubmit={addGoalHandler} >
-    <input type='text' />
+    <input type='text' value={newGoalLvlEnteredText} onChange={textChangeHandler}/>
     <button type='submit'>Add Goal</button>
   </form>
 );
