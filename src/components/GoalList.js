29339d529a0fa0e6bd4e8fc56dfc77f570143a5b
gamerfoxx <admin@josephludbrook.com>
2023-08-10 15:38:08 -0400

    add component

---
+++ src/components/GoalList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './GoalList.css';
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Solidify React Knowledge</li>
+      <li>Solidify React Native Knowledge</li>
+      <li>Obtain new role</li>
+    </ul>
+  );
+};
+
+export default GoalList;
