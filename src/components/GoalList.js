07951d560a05112d4c6e451cd17269be14b1b475
gamerfoxx <admin@josephludbrook.com>
2023-08-10 15:49:26 -0400

    add key

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -6,7 +6,7 @@ const GoalList = (props) => {
   return (
     <ul className="goal-list">{
       props.goals.map(element =>{
-        return <li>{element.text}</li>
+        return <li key={element.id}>{element.text}</li>
       })
     }
     </ul>
